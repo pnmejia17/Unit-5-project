f303acfd21a4fb3714e8dbb736755182d2e68a9a
pnmejia17 <114623502+pnmejia17@users.noreply.github.com>
2023-10-18 14:50:08 -0700

    pre close modal bug fix

--- student-files/js/script.js
+++ student-files/js/script.js
@@ -1,16 +1,20 @@
 // SELECTING HTML ELEMENTS
-const url = 'https://randomuser.me/api/?results=12'
+const ranUserUrl = 'https://randomuser.me/api/?results=12'
 const gallery = document.getElementById('gallery')
+const body = document.querySelector('body')
 
-
+let users = []
 
 // FETCH FUNCTIONS
 
-fetch(url)
-.then(response => response.json())
-.then(data => generateUsers(data.results))
+async function fetchData(url){
+    const response = await fetch('https://randomuser.me/api/?results=12')
+    const data = await response.json()
+    users = data.results
+    generateUsers(data.results)
+}
 
-
+fetchData()
 
 // HELPER FUNCTIONS
 
@@ -37,23 +41,32 @@ gallery.addEventListener('click', (e) => {
   if (userModal) {
     const userName = userModal.querySelector('#name').textContent
     const user = users.find((user) => user.name.first + ' ' + user.name.last === userName)
-    displayUserModal(user)}})
-
-const displayUserModal = (user) => {
-  const modalHTML = `            
-  <div class="modal-container">
-  <div class="modal">
-      <button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>
-      <div class="modal-info-container">
-          <img class="modal-img" src="${user.picture.thumbnail}" alt="profile picture">
-          <h3 id="name" class="modal-name cap">${user.name.first} ${user.name.last}</h3>
-          <p class="modal-text">${user.email}</p>
-          <p class="modal-text cap">${user.city} ${user.state}</p>
-          <hr>
-          <p class="modal-text">(555) 555-5555</p>
-          <p class="modal-text">123 Portland Ave., Portland, OR 97204</p>
-          <p class="modal-text">Birthday: 10/21/2015</p>
-      </div>
-  </div>`;
-  body.insertAdjacentHTML("beforeend", modalHTML); 
+    displayUserModal(user)
 }
+})
+const displayUserModal = (user) => {
+    const modalHTML = `            
+    <div class="modal-container">
+    <div class="modal">
+        <button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>
+        <div class="modal-info-container">
+            <img class="modal-img" src="${user.picture.large}" alt="profile picture">
+            <h3 id="name" class="modal-name cap">${user.name.first} ${user.name.last}</h3>
+            <p class="modal-text">${user.email}</p>
+            <p class="modal-text cap">${user.location.city} ${user.location.state}</p>
+            <hr>
+            <p class="modal-text">${user.cell}</p>
+            <p class="modal-text">${user.location.street.number} ${user.location.street.name}, ${user.location.city}, ${user.location.state} ${user.location.postcode}</p>
+            <p class="modal-text">Birthday: ${user.dob.date} </p>
+        </div>
+    </div>`;
+    body.insertAdjacentHTML("beforeend", modalHTML); 
+  }
+
+body.addEventListener('click', (e) => {
+    const closeButton = e.target.closest('.modal-btn-close')
+    const modalContainer = document.querySelector('.modal-container')
+    if (closeButton){
+      modalContainer.remove()
+}})
+
